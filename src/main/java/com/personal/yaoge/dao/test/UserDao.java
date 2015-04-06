8aa29ad2a8d8faac8910f1f30c6598f2f00ce6a8
yaogenum <yaogemail@gmail.com>
2015-04-06 23:52:19 +0800

    arthectic create ok

--- src/main/java/com/personal/yaoge/dao/test/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.personal.yaoge.dao.test;
-
-import com.personal.yaoge.model.test.User;
-
-public interface UserDao<T> {
-	public User save(T o);
-
-}
--- src/main/java/com/personal/yaoge/service/test/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.personal.yaoge.service.test;
-
-import com.personal.yaoge.model.test.User;
-
-public interface UserService {
-	public User save(User user);
-}
